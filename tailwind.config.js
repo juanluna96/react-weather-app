db403b6933e201091e52901c30489dbf0f8ef232
Juan Diego Luna Castañeda <juandluna17@gmail.com>
2021-07-17 13:35:35 -0500

    Tailwind initial config and styled components

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
 // tailwind.config.js
+const colors = require("tailwindcss/colors");
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'raleway': 'Raleway'
+      },
+      colors: {
+        blueGray: colors.blueGray,
+        coolGray: colors.coolGray,
+        gray: colors.gray,
+        yellow: colors.yellow,
+        trueGray: colors.trueGray
+      }
+    },
   },
   variants: {
     extend: {},
