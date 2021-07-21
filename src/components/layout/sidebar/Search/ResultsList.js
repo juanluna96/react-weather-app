59ddbe054aa6622b29ddda432bb2bbf5f5c1f2c0
Juan Diego Luna Castañeda <juandluna17@gmail.com>
2021-07-21 15:06:14 -0500

    Search result list height fixed

--- src/components/layout/sidebar/Search/ResultsList.js
+++ src/components/layout/sidebar/Search/ResultsList.js
@@ -7,7 +7,7 @@ const ResultsList = () => {
     const searched = useSelector(state => state.search.searched);
 
     return (
-        <ul className="px-5 mt-10">
+        <ul className="max-h-screen px-5 mt-10 overflow-y-scroll">
             {
                 cities.length === 0 && searched === true
                     ? <p className="my-20 text-center">Results not found please try again.</p>
